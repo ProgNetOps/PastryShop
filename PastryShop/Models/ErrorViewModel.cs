1b13749c8caac7293bbb117895074e1daa74d076
Suberu 'Seyi <suberuseyi@gmail.com>
2025-06-18 15:56:31 +0100

    Pie and Category entity models created

--- PastryShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PastryShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
